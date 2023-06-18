42ac91a7b2f5fefadc4778c035c2ad5b52f09b6a
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-18 23:08:01 +0900

    practice @ts-check

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,20 @@
+// @ts-check
+/**
+ * Initializes the project TS파일이 JS를 체크해주네...
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function init(config) {
 	return true;
 }
+
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns {number}
+ */
 export function exit(code) {
-	return code + 1
+	return code + 1;
 }
